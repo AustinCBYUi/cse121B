e7682a243e1aa15c65df48c2e47a93fb71d85600
AustinCBYUi <131830841+AustinCBYUi@users.noreply.github.com>
2023-09-19 17:42:11 -0700

    Javascript is sweet!

---
+++ sandbox/dom-basics.js
@@ -0,0 +1,24 @@
+//Create a new paragraph.
+const newParagraph = document.createElement("p");
+
+//Ooooh, changes the new paragraph element text to added w/ js then
+newParagraph.innerText = "Added with JavaScript!";
+//adds paragraph to the document!
+document.body.appendChild(newParagraph);
+
+
+//Create new image
+const newImage = document.createElement("img");
+
+//Add image
+newImage.setAttribute("src", "https://picsum.photos/200");
+newImage.setAttribute("alt", "This is a picture of something but I'm not too sure!")
+//Add to document
+document.body.appendChild(newImage);
+
+
+//new section
+const newSection = document.createElement("section");
+
+newSection.innerHTML = "<h2>CSE 121B</h2><p>Welcome to JavaScript!</p>";
+document.body.appendChild(newSection);
