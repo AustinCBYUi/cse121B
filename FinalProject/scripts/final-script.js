3d5997caf2e5238fa64b7420dec9336ea940d14a
AustinCBYUi <131830841+AustinCBYUi@users.noreply.github.com>
2023-10-22 11:33:54 -0700

    Testing Media, refactored to requirements

--- FinalProject/scripts/final-script.js
+++ FinalProject/scripts/final-script.js
@@ -37,9 +37,17 @@ const displayBooks = async (books) => {
 
 
 //Reset function to erase items.
+//This throws an 'error' in the DOM because once it's called it removes the item, so it says
+//it is null.
+//Fixed the previous error by using the conditional.
 async function reset() {
     let erase = await document.querySelector("#grid-item-right");
-    erase.remove();
+    if (erase == "#grid-item-right") {
+        erase.remove();
+    }
+    else {
+        console.log("Erased previous items.")
+    }
     // erase.innerHTML = null;
 }
 
